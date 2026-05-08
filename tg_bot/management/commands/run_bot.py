efc137c66dc00fc0e341c7efdccea0c699f48b8c
kiqiou <kuksare@gmail.com>
2026-05-08 19:45:08 +0300

    feat: add try catch for bot start

--- tg_bot/management/commands/run_bot.py
+++ tg_bot/management/commands/run_bot.py
@@ -5,8 +5,15 @@ from tg_bot.initial import bot
 import tg_bot.handlers
 import tg_bot.signals
 
+import time
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("🚀 Запуск Telegram бота...")
 
-        bot.infinity_polling(skip_pending=True)
+        while True:
+            try:
+                bot.infinity_polling(skip_pending=True)
+            except Exception as e:
+                logger.exception(e)
+                time.sleep(5)
